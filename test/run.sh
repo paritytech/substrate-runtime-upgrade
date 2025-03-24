6997e86535380a4c466d1b4d285044164c98c03c
BulatSaif <bulat@parity.io>
2025-03-24 19:03:22 +0300

    add dry run mode

--- test/run.sh
+++ test/run.sh
@@ -11,6 +11,7 @@ export INPUT_WASMPATH="test/data/new-wasm"
 #export INPUT_WASMPATH="https://github.com/paritytech/polkadot-sdk/releases/download/polkadot-stable2412/asset_hub_westend_runtime.compact.compressed.wasm"
 export INPUT_ACCOUNT="//Alice"
 export INPUT_RELAYCHAINURL="ws://test_node_alice_1:9944"  # optional, if needed
+export INPUT_DRYRUN="false"  # optional, if needed
 
 #export  SUBWASM_VERSION=0.19.1
 #wget https://github.com/chevdor/subwasm/releases/download/v${SUBWASM_VERSION}/subwasm_linux_amd64_v${SUBWASM_VERSION}.deb
